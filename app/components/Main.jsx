03ee0383cda0ed673b6dca8b47036e8ab170fbf7
Kyle Crichton <kylecrichton@gmail.com>
2017-02-20 16:19:55 -0600

    changing boilerplate

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,29 +1,12 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
-
-class Main extends Component {
-
-  render: function() {
-    return (
-      <div className="font main">
-        <Nav/>
-        <div className="page-actions"></div>
-        <div className="row" id="logorow">
-          <div className="columns medium-3 large-2 small-centered">
-            <LogoLeft/>
-          </div>
-          <div className="columns medium-6  large-8 small-centered">
-            <ChatRoom></ChatRoom>
-          </div>
-          <div className="columns medium-3 large-2 small-centered">
-            <LogoRight/>
-          </div>
-        </div>
-        <Footer/>
+class Main extends Component{
+  render(){
+    return(
+      <div>
       </div>
-    );
+    )
   }
-};
+}
 
-// module.exports = Main;
 export default Main;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component }  from 'react';
 /*how the routes are defined, using inde link show the main link selected only when it is required*/
 var {Link, IndexLink} = require('react-router');
 
