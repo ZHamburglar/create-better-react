4866b66bcd65c4ff236859f0b2e6ec58ae2feec7
Kyle Crichton <kylecrichton@gmail.com>
2017-02-20 16:48:08 -0600

    changed footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,20 +1,14 @@
-var React = require('react');
-/*how the routes are defined, using inde link show the main link selected only when it is required*/
-var Footer = React.createClass({
-  render : function(){
-    return (
-      <footer className="challenge-footer">
-      <div className="font main">
-        <h6 className="foot createdby">
-          <a href="https://github.com/kcricht06"><img border="0" alt="Kyle Crichton" src="app/images/evanderholyfield.png" className="image img-circle" width="100" height="100"/></a>
-          <a href="https://github.com/ZHamburglar"><img border="0" alt="Kollin Brandenburg" src="app/images/miketyson.png" className="image img-circle" width="100" height="100" /></a>
-          <a href="https://github.com/Luke-Popwell"><img border="0" alt="Luke Popwell" src="app/images/oscardelahoya.png" className="image img-circle" width="100" height="100" /></a>
-          <a href="https://github.com/snlandis"><img border="0" alt="Stephen Landis" src="app/images/royjonesjr.png" className="image img-circle" width="100" height="100" /></a>
-        </h6>
-      </div>
-      </footer>
-    );
-  }
-});
+import React, { Component } from 'react';
 
-module.exports = Footer;
+class Footer extends Component {
+
+  render(){
+    return(
+      <div>
+        <p>8====D</p>
+      </div>
+    )
+  }
+}
+
+export default Footer;
