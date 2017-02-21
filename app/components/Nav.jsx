909ff56540cdc870320aa3e7b4752704e1ab29d1
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-02-20 18:24:00 -0600

    removed extra files

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,40 +5,25 @@ var {Link, IndexLink} = require('react-router');
 import * as Redux from 'react-redux';
 import * as actions from 'actions';
 import router from 'app/router/';
-import Header from './ChatBox/Header.jsx'
 
 var Nav = React.createClass({
-  onSearch: function(e){
-    e.preventDefault();
-    var location = this.refs.location.value;
-    var encodedLocation = encodeURIComponent(location);
-    if (location.length > 0){
-      this.refs.location.value= '';
-      window.location.hash = '#/?location=' + encodedLocation;
-    }
-  },
-
   render() {
     return(
-<div>
-			<div className="title-bar" id="navbar" data-responsive-toggle="example-menu" data-hide-for="medium">
-			</div>
-
-      <nav className="top-bar" role="navigation">
-          <ul className="small-block-grid-4" data-dropdown-menu>
-            <li>
-              <Link to="/main" className="eachLink" activeClassName="active" activeStyle={{fontWeight:'bold',color:'red'}}>TRIVIA FIGHT</Link>
-            </li>
-            <li>
-              <Link to="/challenge" className="eachLink" activeClassName="active" activeStyle={{fontWeight:'bold',color:'red'}}>CHALLENGE</Link>
-            </li>
-					</ul>
-
-      </nav>
+      <div>
+        <nav className="top-bar" role="navigation">
+            <ul className="small-block-grid-4" data-dropdown-menu>
+              <li>
+                <Link to="/main" className="eachLink" activeClassName="active" activeStyle={{fontWeight:'bold',color:'red'}}>TRIVIA FIGHT</Link>
+              </li>
+              <li>
+                <Link to="/challenge" className="eachLink" activeClassName="active" activeStyle={{fontWeight:'bold',color:'red'}}>CHALLENGE</Link>
+              </li>
+  					</ul>
+        </nav>
 		</div>
     );
   }
-});
+  });
 
 module.exports = Nav;
 // export default Redux.connect()(Nav);
--- app/router/index.jsx
+++ app/router/index.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
-import Login from 'Login';
-import firebase from 'app/firebase/';
 
 import Main from 'Main';
 import Challenge from 'Challenge';
 
-
-// var requireLogin = (nextState, replace, next) => {
-//   if (!firebase.auth().currentUser) {
-//     replace('/main');
-//   }
-//   next();
-// };
-//
-// var redirectIfLoggedIn = (nextState, replace, next) => {
-//   if (firebase.auth().currentUser) {
-//     replace('/main');
-//   }
-//
-//   next();
-// };
 
 
 export default (
@@ -33,17 +16,3 @@ export default (
     </Route>
   </Router>
 );
-
-
-
-// export default (
-//   <Router history={hashHistory}>
-//     <Route path="/">
-//       <Route path="main" component={Main} onEnter={requireLogin}/>
-// 			<Route path="Challenge" component={Challenge}/>
-// 			<Route path="Leaderboard" component={Leaderboard}/>
-// 			<Route path="Gameroom" component={Gameroom}/>
-//       <IndexRoute component={Login} onEnter={redirectIfLoggedIn}/>
-//     </Route>
-//   </Router>
-// );
